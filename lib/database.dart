c5a6671db7d280ff41550858471fe60742c099c7
Fabian Fasse <fabianfasse@icloud.com>
2022-12-18 23:00:32 +0100

    add database migrations

---
+++ lib/database.dart
@@ -0,0 +1,23 @@
+import 'package:sqflite/sqlite_api.dart';
+import 'package:sqflite_migration/sqflite_migration.dart';
+
+final _initialScript = [
+  '''
+    create table fees ( 
+    id integer primary key autoincrement, 
+    base_fee real not null,
+    price_per_unit real not null,
+    monthly_discount real not null)
+  '''
+];
+
+final List<String> _migrations = [];
+final _config = MigrationConfig(
+  initializationScript: _initialScript,
+  migrationScripts: _migrations,
+);
+
+Future<Database> initDatabase() async {
+  const dbPath = 'qum.db';
+  return await openDatabaseWithMigration(dbPath, _config);
+}
--- lib/di.dart
+++ lib/di.dart
@@ -1,5 +1,6 @@
 import 'package:get_it/get_it.dart';
 import 'package:qum/data/repositories/fee_repository_impl.dart';
+import 'package:qum/database.dart';
 import 'package:qum/domain/repositories/fee_repository.dart';
 import 'package:qum/domain/use_cases/create_fee.dart';
 import 'package:qum/domain/use_cases/get_fees.dart';
@@ -11,19 +12,7 @@ final locator = GetIt.instance;
 
 void init() {
   locator.registerSingletonAsync<FeeRepository>(() async {
-    Database database = await openDatabase(
-      'qum.db',
-      version: 1,
-      onCreate: (db, version) async {
-        await db.execute('''
-          create table fees ( 
-            id integer primary key autoincrement, 
-            base_fee real not null,
-            price_per_unit real not null,
-            monthly_discount real not null)
-        ''');
-      },
-    );
+    Database database = await initDatabase();
 
     return FeeRepositoryImpl(database: database);
   });
